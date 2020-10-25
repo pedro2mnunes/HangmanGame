b42e9ca8b6447a2263753289e7086d8807e9b53c
Pedro Nunes <pedro2mnunes@hotmail.com>
2020-10-25 11:41:34 +0000

    Console application created

---
+++ HangmanGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HangmanGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
