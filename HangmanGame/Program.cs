830c7afe2a3fe5bf062479ed480305b20762a35d
Pedro Nunes <pedro2mnunes@hotmail.com>
2020-10-25 12:22:21 +0000

    Variable declaration.

--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace HangmanGame
 {
@@ -6,7 +10,14 @@ namespace HangmanGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           //Instantiate random number generator using system-supplied value as seed.
+           var rand = new Random();
+           //The string Input will store the user input.
+           string Input;
+           //The string Solution stores the solution word.
+           string Solution;
+           // The bool HasFinished stores the player status in the game. While false means the the player has not win/finished the game. 
+            bool HasFinished = false;
         }
     }
 }
